1c9cd552e6fc45ea5488481c1e4b7c09dd3e539c
DungNV101 <DungNV101@fpt.com>
2024-03-28 14:24:55 +0700

    add script PlayerMovement

---
+++ Assets/3DGladiatorFight/Scripts/Helpers/TagManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis
+{
+    public static string HORIZONTAL_AXIS = "Horizontal";
+    public static string VERTICAL_AXIS = "Vertical";
+}
---
+++ Assets/3DGladiatorFight/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    public float movement_Speed = 3f;
+    public float gravity = 9.8f;
+    public float rotation_Speed = 0.15f;
+    public float rotateDegreesPerSecond = 180f;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetAxis(Axis.VERTICAL_AXIS) > 0)
+        {
+            Vector3 moveDirection = transform.forward;
+            moveDirection.y -= gravity * Time.deltaTime;
+
+            characterController.Move(moveDirection * movement_Speed * Time.deltaTime);
+        }
+        else if (Input.GetAxis(Axis.VERTICAL_AXIS) < 0)
+        {
+            Vector3 moveDirection = -transform.forward;
+            moveDirection.y -= gravity * Time.deltaTime;
+
+            characterController.Move(moveDirection * movement_Speed * Time.deltaTime);
+        }
+    }
+}
