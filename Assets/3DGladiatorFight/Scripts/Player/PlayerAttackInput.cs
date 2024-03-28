17699562e723045afce25c68a3bce1b825203bf3
DungNV101 <DungNV101@fpt.com>
2024-03-28 16:54:02 +0700

    add script Attack

---
+++ Assets/3DGladiatorFight/Scripts/Player/PlayerAttackInput.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackInput : MonoBehaviour
+{
+    private CharacterAnimations playerAnimation;
+
+    void Awake()
+    {
+        playerAnimation = GetComponent<CharacterAnimations>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // defend when J pressed
+        PressedDefend();
+        PressedAttack();
+    }
+
+    void PressedDefend()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            playerAnimation.Defend(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            playerAnimation.Defend(false);
+        }
+    }
+
+    void PressedAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (Random.Range(0, 2) > 0)
+            {
+                playerAnimation.Attack_1();
+            }
+            else
+            {
+                playerAnimation.Attack_2();
+            }
+        }
+    }
+}
